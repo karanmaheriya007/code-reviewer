d1bd90d92f7235b3f8c8fa53505cffd5d97b2ac1
aamena <aamena.rangwala@tectrionix.com>
2025-03-02 18:08:56 +0530

    change App.jsx

--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -1,13 +1,17 @@
 const generateContent = require("../services/ai.service");
 
 module.exports.getReview = async(req,res)=>{
-    const code = req.body.code;
+    try {
+        const code = req.body.code;
 
-    if(!code){
-        return res.status(400).json({error: 'Prompt is required'});
+        if(!code){
+            return res.status(400).json({error: 'Prompt is required'});
+        }
+
+        const response = await generateContent(code);
+
+        res.send(response);
+    } catch (error) {
+        res.status(500).json({error: 'An error occurred while processing your request'});
     }
-
-    const response = await generateContent(code);
-
-    res.send(response);
 }
